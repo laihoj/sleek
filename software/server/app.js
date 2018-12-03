d9f4a0d10ab4ccbf4173d96facadbd535af82589
Jaakko Laiho <jaakkolaiho94@gmail.com>
2018-12-03 08:11:27 +0200

    revert

--- software/server/app.js
+++ software/server/app.js
@@ -22,8 +22,8 @@ app.use(require("express-session")({
 app.use(passport.initialize());
 app.use(passport.session());
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended:true}));
 app.set('views', __dirname + '/views/');
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 app.use(flash());
 
@@ -40,7 +40,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-var url = process.env.DATABASEURL || "mongodb://Admin:admin1@ds123624.mlab.com:23624/sleek";
+var url = process.env.DATABASEURL;
 mongoose.connect(url, { useNewUrlParser: true });
 
 var domain = process.env.DOMAIN || "localhost:3000";
@@ -60,7 +60,7 @@ app.post("/users", function(req,res){
 			res.redirect("/");
 		} else {
 			passport.authenticate("local")(req, res, function() {
-				res.redirect(req.session.redirectTo || '/users/' + req.user.username);
+				res.redirect(req.session.redirectTo || '/users/' +req.user.username);
 				delete req.session.redirectTo;
 			}
 		)}
@@ -77,7 +77,7 @@ app.post("/login", passport.authenticate("local", {failureRedirect: "/login", fa
 		res.redirect("/login");
 	} else {
 		req.flash("success", "Logged in");
-		res.redirect(req.session.redirectTo || '/users/' + req.user.username);
+		res.redirect(req.session.redirectTo || '/users/' +req.user.username);
 		delete req.session.redirectTo;
 	}
 });
