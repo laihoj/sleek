1297cc20b712797c8042f41d7f03ff4a2e94e62d
Jaakko Laiho <jaakkolaiho94@gmail.com>
2018-12-06 13:16:01 +0100

    view directory fix

--- software/server/routes/devices.js
+++ software/server/routes/devices.js
@@ -3,7 +3,7 @@ var express = require("express"),
 
 var Device = require("../models/device");
 const db = require('../db.js');
-
+router.set('views', __dirname + '/views/');
 router.get("/", async function(req,res) {
 	let devices = await db.devices();
 	if(devices) {
